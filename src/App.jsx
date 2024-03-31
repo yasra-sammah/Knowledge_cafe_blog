30eac13548a8dafb18c9a7bacc00cf09d6c355b2
yasra-sammah <yasra.sammah.sam@gmail.com>
2024-04-01 02:50:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+    <Header></Header>
+    <Blogs></Blogs>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Knowledge Blog</h1>
+    </div>
+  );
+};
+
+export default Header;
